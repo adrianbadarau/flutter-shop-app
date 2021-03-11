82263d754c0a1c37aace54889bf2a5a0c7bd14a3
Adrian Badarau <adi.badarau@gmail.com>
2021-03-11 19:54:56 +0100

    started implementing the cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/providers/cart_provider.dart';
 import 'package:shop/providers/products_provider.dart';
 import 'package:shop/screens/product_detail_screen.dart';
 import 'package:shop/screens/products_screen.dart';
@@ -9,8 +10,11 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (ctx) => ProductsProvider()),
+        ChangeNotifierProvider(create: (ctx) => Cart_Provider()),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
