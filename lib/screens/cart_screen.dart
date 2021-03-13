a70eec8458be30f758439229e445b79a3c32017b
Adrian Badarau <adi.badarau@gmail.com>
2021-03-13 11:37:18 +0100

    added cart items

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/providers/cart_provider.dart';
+import 'package:shop/providers/cart_provider.dart' show CartProvider;
+import 'package:shop/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = "/cart";
@@ -42,7 +43,21 @@ class CartScreen extends StatelessWidget {
                 ],
               ),
             ),
-          )
+          ),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (context, index) {
+              var item = cartProvider.items.values.toList()[index];
+              return CartItem(
+                id: item.id,
+                price: item.price,
+                qty: item.quantity,
+                title: item.title,
+              );
+            },
+            itemCount: cartProvider.itemCount,
+          ))
         ],
       ),
     );
